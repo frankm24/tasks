b684e7e5441a7a844c39d1fe4ce1f2a9eccb48b6
frankm24 <murphyfr@udel.edu>
2026-02-09 00:15:36 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>
+                FRANK MURPHY has entered the chat.
+            </p>
         </div>
     );
 }
